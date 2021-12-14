cccf6c8c0a1ba70e5bbbb6cb22fcd5671858e852
shubam.virdi <developershubam@gmail.com>
2021-12-14 19:56:17 +0530

    Added: About us amendments

--- app/src/main/java/cf/projectspro/bank/ui/activities/AboutUs.java
+++ app/src/main/java/cf/projectspro/bank/ui/activities/AboutUs.java
@@ -3,6 +3,8 @@ package cf.projectspro.bank.ui.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -12,33 +14,28 @@ import android.widget.Toast;
 import cf.projectspro.bank.R;
 
 public class AboutUs extends AppCompatActivity {
-    private TextView mEmaildev;
+
+    private TextView versionName, buildCode;
+    private String mVersion, mBuild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
 
-        mEmaildev = findViewById(R.id.emaildev);
-        mEmaildev.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        //Initialise UI Variables.
+        versionName = findViewById(R.id.versionname);
+        buildCode = findViewById(R.id.buildcode);
 
-                Intent i = new Intent(Intent.ACTION_SEND);
-                i.setData(Uri.parse("mailto:"));
-                i.setType("text/plain");
-                i.putExtra(Intent.EXTRA_EMAIL, new String[]{"shubhamvirdic3@gmail.com"});
-                i.putExtra(Intent.EXTRA_SUBJECT, "Mail to Developer");
-
-                try {
-                    startActivity(Intent.createChooser(i, "sendmail"));
-                    finish();
-                } catch (Exception e) {
-                    Toast.makeText(AboutUs.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                }
-
-
-            }
-        });
+        //Get Version and Build of App.
+        try {
+            PackageInfo pInfo = getApplicationContext().getPackageManager().getPackageInfo(getPackageName(), 0);
+            mVersion = pInfo.versionName;
+            mBuild = pInfo.versionCode + "";
+            versionName.setText("v" + mVersion);
+            buildCode.setText("Build " + mBuild);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
