9fbc1f5123ab67fccf4544c5277d5d00bbf42ff4
Shubam Virdi <shubhamvirdic3@gmail.com>
2022-01-11 21:16:48 +0530

    Improved: Login and Sign up Flow

--- app/src/main/java/cf/projectspro/bank/ui/activities/Signup.java
+++ app/src/main/java/cf/projectspro/bank/ui/activities/Signup.java
@@ -3,125 +3,148 @@ package cf.projectspro.bank.ui.activities;
 import android.app.ProgressDialog;
 import android.content.Intent;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.View;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import cf.projectspro.bank.R;
+import cf.projectspro.bank.databinding.ActivitySignupBinding;
+import cf.projectspro.bank.ui.viewModels.SignUpViewModel;
 
 public class Signup extends AppCompatActivity {
-    private EditText name, email, pass;
-    private Button signup, login;
-    private FirebaseAuth mAuth;
-    private String uid;
-    private ProgressDialog pd;
+
+    private ProgressDialog signUpProgressDialog;
+    private ActivitySignupBinding binding;
+    private SignUpViewModel viewModel;
+    public static final String TAG = Signup.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivitySignupBinding.inflate(LayoutInflater.from(this));
+        setContentView(binding.getRoot());
 
-        setContentView(R.layout.activity_signup);
-        name = findViewById(R.id.name_signup);
-        email = findViewById(R.id.email_signup);
-        pass = findViewById(R.id.pass_signup);
-        signup = findViewById(R.id.signup_now);
-        login = findViewById(R.id.to_login_screen);
-        pd = new ProgressDialog(this);
-        pd.setMessage("Please Wait ...");
-        pd.hide();
-        mAuth = FirebaseAuth.getInstance();
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Signup.this, Login.class);
+
+        viewModel = new ViewModelProvider(this).get(SignUpViewModel.class);
+
+        viewModel.getStatus().observe(this,success->{
+            if (success){
+
+                Log.e(TAG, "getStatus: SignUp: Success");
+                Intent intent = new Intent(Signup.this, MainActivity.class);
                 startActivity(intent);
                 finish();
+                dismissSignUpProgressDialog();
+
+            }else{
+                Log.e(TAG, "getStatus: SignUp: Failed");
             }
-
-
         });
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                signup_the_user();
-            }
+
+        initializeProgressDialog();
+        hideSignUpProgressDialog();
+
+        binding.toLoginScreen.setOnClickListener(view -> {
+            Intent intent = new Intent(Signup.this, Login.class);
+            startActivity(intent);
+            finish();
+        });
+
+        binding.signupNow.setOnClickListener(v->{
+            signUpUser();
         });
 
     }
 
+    private void initializeProgressDialog() {
+        signUpProgressDialog = new ProgressDialog(this);
+        signUpProgressDialog.setMessage("Please Wait ...");
+    }
 
-    private void signup_the_user() {
+    public void showSignUpProgressDialog(){
+        signUpProgressDialog.show();
+    }
 
-        pd.show();
-        final String Name = name.getText().toString().trim();
-        final String Pass = pass.getText().toString().trim();
-        final String Email = email.getText().toString().trim();
+    public void hideSignUpProgressDialog(){
+        signUpProgressDialog.hide();
+    }
 
-        // Toast.makeText(this, Email+" "+pass, Toast.LENGTH_SHORT).show();
-        if (!TextUtils.isEmpty(Name) && !TextUtils.isEmpty(Email) && !TextUtils.isEmpty(Pass) && Pass.length() > 6) {
-
-
-            if (!Email.contains("@") && !Email.contains(".")) {
-                Toast.makeText(this, "Enter valid email!", Toast.LENGTH_SHORT).show();
-            } else {
-                mAuth.createUserWithEmailAndPassword(Email, Pass).addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        Toast.makeText(Signup.this, "Successful", Toast.LENGTH_SHORT).show();
-                        mAuth.signInWithEmailAndPassword(Email, Pass).addOnCompleteListener(task1 -> {
-                            if (task1.isSuccessful()) {
-
-                                uid = mAuth.getCurrentUser().getUid();
-
-                                // Toast.makeText(Signup.this, "" + uid, Toast.LENGTH_SHORT).show();
-
-                                DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
-                                DatabaseReference isadmin = FirebaseDatabase.getInstance().getReference().child("uids").child(uid);
-                                isadmin.setValue(false);
-                                ref.child("name").setValue(Name);
-                                ref.child("amount").setValue(0);
-                                ref.child("uid").setValue(uid);
-                                ref.child("session").setValue(true);
-                                isadmin.setValue(false);
-
-                                Intent intent = new Intent(Signup.this, MainActivity.class);
-                                startActivity(intent);
-                                finish();
-                                pd.dismiss();
-
-                            } else {
-
-                                Intent intent = new Intent(Signup.this, Login.class);
-                                startActivity(intent);
-                                finish();
-                                pd.dismiss();
-                            }
-                        });
+    public void dismissSignUpProgressDialog(){
+        signUpProgressDialog.dismiss();
+    }
 
 
-                    } else {
-                        Toast.makeText(Signup.this, "Sign up Failed", Toast.LENGTH_SHORT).show();
-                        pd.dismiss();
-                    }
+    /**
+     * function to initiate sign up process
+     */
+    private void signUpUser() {
 
-                });
+        showSignUpProgressDialog();
+
+        final String Name = binding.nameSignup.getText().toString().trim();
+        final String Email = binding.emailSignup.getText().toString().trim();
+        final String Pass = binding.passSignup.getText().toString().trim();
+
+        if (!TextUtils.isEmpty(binding.nameSignup.getText().toString().trim()) && !TextUtils.isEmpty(binding.emailSignup.getText().toString()) && !TextUtils.isEmpty(binding.passSignup.getText().toString().trim())) {
+
+            if (!validateUserFullName(Name)){
+                dismissSignUpProgressDialog();
+                Toast.makeText(this, "Name is not valid!", Toast.LENGTH_SHORT).show();
+            }else if (!validateUserEmail(Email)){
+                dismissSignUpProgressDialog();
+                Toast.makeText(this, "Email address not valid!", Toast.LENGTH_SHORT).show();
+            }else if (Pass.length() < 6){
+                dismissSignUpProgressDialog();
+                Toast.makeText(this, "Password Length less than 6", Toast.LENGTH_SHORT).show();
+            }else{
+                Log.e(TAG, "SignUpUser: valid details:");
+                viewModel.signUpUserWithEmailAndPassword(Name,Email,Pass);
             }
         } else {
-            Toast.makeText(this, "Fill All Details With Password Length Greater Than 6", Toast.LENGTH_SHORT).show();
-            pd.dismiss();
+            Toast.makeText(this, "Fill All Details", Toast.LENGTH_SHORT).show();
+            dismissSignUpProgressDialog();
         }
 
+    }
+
+    /**
+     * function to validate user full name
+     * @param name name of the user
+     * @return true if valid otherwise false
+     */
+    public static boolean validateUserFullName(String name) {
+
+        /*validates normal name and
+        Peter Müller
+        François Hollande
+        Patrick O'Brian
+        Silvana Koch-Mehrin types of names
+         */
+        String regex = "^[\\p{L} .'-]+$";
+        Pattern pattern = Pattern.compile(regex,Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(name);
+        return matcher.find();
+    }
+
+
+    /**
+     * function to validate user email address
+     * @param email name of the user
+     * @return true if valid otherwise false
+     */
+    public static boolean validateUserEmail(String email) {
+        return email.contains("@") && email.contains(".");
     }
 }
